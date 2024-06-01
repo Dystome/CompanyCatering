399893d8735de4503696b9fe4b840a72ec5a782b
dystome <atienferra@gmail.com>
2024-06-01 10:22:49 +0200

    First Commit

---
+++ CompanyCatering/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CompanyCatering.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
