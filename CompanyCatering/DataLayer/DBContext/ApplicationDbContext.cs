1b1faf8d53acd49de0e8daf6a53d9ad36b8b9c9c
dystome <atienferra@gmail.com>
2024-06-01 10:55:38 +0200

    Test Commit

---
+++ CompanyCatering/DataLayer/DBContext/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CompanyCatering.DataLayer.DBContext
+{
+    public class CompanyCateringDbContext : IdentityDbContext
+    {
+        public CompanyCateringDbContext()
+        {
+        }
+
+        public CompanyCateringDbContext(DbContextOptions<CompanyCateringDbContext> options) : base(options)
+        {
+        }   
+    }
+}
---
+++ CompanyCatering/DataLayer/Entities/FoodCategory.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CompanyCatering.DataLayer.Entities
+{
+    public class FoodCategory
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+    }
+}
